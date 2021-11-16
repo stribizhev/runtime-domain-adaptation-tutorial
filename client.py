a427a5903021d4ebebe9215a0bc72792393bfbc5
Rihards Krišlauks <rihards.krislauks@gmail.com>
2021-11-16 15:04:55 +0200

    Add help messages and the option to only translate (not translate and update)

--- client.py
+++ client.py
@@ -44,15 +44,27 @@ def translate_and_save(url, sources, targets, uuid):
 
     delete_all_updates(uuid)
 
+def only_translate(url, sources, uuid):
+    delete_all_updates(uuid)
+
+    for source, target in sources:
+        translation = translate(source, uuid)
+        print(translation)
 
 
 if __name__ == "__main__":
     parser = ag.ArgumentParser(description="Translates files by making requests to a runtime domain adaptation webservice. See https://github.com/marian-cef/marian-examples/tree/master/adaptive")
-    parser.add_argument('url', metavar='TRANSLATION_SERVICE_URL')
-    parser.add_argument('-s', '--source', type=ag.FileType('r', encoding='utf-8'), required=True)
-    parser.add_argument('-t', '--target', type=ag.FileType('r', encoding='utf-8'), required=True)
-    parser.add_argument('-u', '--uid', required=True)
+    parser.add_argument('url', metavar='TRANSLATION_SERVICE_URL',
+                        help="URL to the runtime domain adaptation webservice")
+    parser.add_argument('-s', '--source', type=ag.FileType('r', encoding='utf-8'), required=True,
+                        help="The file to translate")
+    parser.add_argument('-t', '--target', type=ag.FileType('r', encoding='utf-8'), required=False,
+                        help="A file containing reference translations. Used to simulate a translator who's sending in post-edited translations.")
+    parser.add_argument('-u', '--uid', required=True, help="Provide a random string to identify your sent in sentences.")
 
     args = parser.parse_args()
 
-    translate_and_save(args.url, args.source, args.target, args.uid)
+    if args.target is None:
+        only_translate(args.url, args.source, args.uid)
+    else:
+        translate_and_save(args.url, args.source, args.target, args.uid)
